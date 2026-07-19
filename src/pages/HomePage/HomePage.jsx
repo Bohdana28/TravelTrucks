5b7cafa21208071374124a722d5892b4e1e82934
Bohdana Olkhovetska <bog.olhowetzcka@gmail.com>
2026-07-19 12:38:19 +0100

    add style for HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,24 @@
-
+import { Link } from "react-router-dom";
+import css from "./HomePage.module.css";
 
 
 export default function HomePage() {
-    
+  return (
+      <section className={css.hero}>
+          <div className="container">
+        <div className={css.homePage}>
+          <div className={css.taxtAndtitle}>
+              <h1 className={css.mainTitle}>Campers of your dreams</h1>
+              <p className={css.text}>You can find everything you want in our catalog</p>
+          </div>
+                
+                <Link to='/catalog'
+                className={css.viewbtn}
+                >View Now</Link>
+              </div>
+          </div>
+          
+
+    </section>
+  );
 }
